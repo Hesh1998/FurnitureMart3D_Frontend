57906e4e078d404a06a696bc72e552a630f3c55d
Heshan Nanayakkara <kalaruheshan@gmail.com>
2022-12-17 17:27:36 +0530

    3D demo

--- test/new3D/test.js
+++ test/new3D/test.js
@@ -4,44 +4,81 @@ import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm//
 
 const canvas = document.querySelector('.webgl');
 const scene = new THREE.Scene();
-
-
 const loader = new GLTFLoader();
-loader.load('uploads_files_3902233_sofa(1).glb', function(glb){
-    const root = glb.scene;
-    root.scale.set(1.5, 1.5, 1.5);
-    scene.add(root);
-},function ( xhr ) {
-    console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-})
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(2, 2, 5);
-scene.add(light);
+const set3DModel = async() => {
+    document.body.style.background = "white";
+    var root;
 
-// Boiler plate code
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+    
+    loader.load('uploads_files_3902233_sofa(1).glb', function(glb){
+        root = glb.scene;
+        root.scale.set(1.5, 1.5, 1.5);
+        scene.add(root);
+    },function ( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+    })
+    
+    // Adding light
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(2, 2, 5);
+    scene.add(light);
+    
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100);
-camera.position.set(0, 1, 2);
-scene.add(camera)
+    // Setting sizes
+    const sizes = {
+        width: window.innerWidth/1.3,
+        height: window.innerHeight
+    }
+    
+    
+    // Adding the camera 
+    // (Width camera sees in degress, aspect ratio - entire widow, min distance to see an object, max distance to seen an object)
+    const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100);
+    camera.position.set(0, 0.5, 2);
+    scene.add(camera)
+    
 
-const renderer = new THREE.WebGL1Renderer({
-    canvas: canvas
-})
-
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
-renderer.gammaOutput = true;
-renderer.render(scene, camera);
-
-function animate(){
-    requestAnimationFrame(animate);
+    // Defining and setting the renderer
+    const renderer = new THREE.WebGL1Renderer({
+        canvas: canvas
+    })
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.shadowMap.enabled = true;
+    renderer.gammaOutput = true;
     renderer.render(scene, camera);
+    
+
+    // Setting mouse coordinates to rotate the 3D object with the mouse move
+    var mx = 0;
+    var my = 0;
+
+    function saveMouse(event){
+        let mxTemp = event.clientX;
+        var myTemp = event.clientY;
+        var el = document.elementFromPoint(mxTemp, myTemp);
+        
+        if(el == canvas){
+            mx = mxTemp;
+            my = myTemp;
+        }
+    }
+    document.onmousemove = saveMouse;
+
+
+    // Define and run animate function
+    const animate = async() => {
+        requestAnimationFrame(animate);
+
+        // rotating the 3d object
+        root.rotation.y = mx/125; 
+        root.rotation.x = my/100;
+
+        renderer.render(scene, camera);
+    }
+    
+    animate();
 }
 
-animate();
+set3DModel();
