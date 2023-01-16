0259cfd1d5ebd1790a27b57445d221f9ac3e2925
Heshan Nanayakkara <kalaruheshan@gmail.com>
2023-01-16 14:56:23 +0530

    Alerts completed upto now

--- assets/alert-box/alert-script.js
+++ assets/alert-box/alert-script.js
@@ -1,3 +1,4 @@
+// Open toast
 function toastSetup(){
     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
     var toastList = toastElList.map(function(toastEl) {
@@ -7,19 +8,32 @@ function toastSetup(){
 }
 
 
+// Close toast
+function toastClose(){
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function(toastEl) {
+        return new bootstrap.Toast(toastEl)
+    })
+    toastList.forEach(toast => toast.hide()) 
+}
+
+
+// Open error toat
 function errorToast(message){
-    document.querySelector(".toast").innerHTML  = `<div class="toast-header" style="background-color: #f1aeae;"> <img src="alert-24.png" class="me-auto"> <button type="button" class="btn-close" data-bs-dismiss="toast"></button> </div> <div class="toast-body" style="background-color: white; border-radius: 10px;"> <h5 style="text-align: center;">${message}</h5> </div>`;
+    document.querySelector(".toast").innerHTML  = `<div class="toast-header" style="background-color: #f1aeae;"> <img src="alert-24.png" class="me-auto"> <button type="button" class="btn-close" data-bs-dismiss="toast"></button> </div> <div class="toast-body" style="background-color: white; border-radius: 10px;"> <h5 style="text-align: center; font-size: 16px;">${message}</h5> </div>`;
     toastSetup();
 }
 
 
+// Open information toat
 function infoToast(message){
-    document.querySelector(".toast").innerHTML  = `<div class="toast-header" style="background-color: #c4ff89;"> <img src="ok-24.png" class="me-auto"> <button type="button" class="btn-close" data-bs-dismiss="toast"></button> </div> <div class="toast-body" style="background-color: white; border-radius: 10px;"> <h5 style="text-align: center;">${message}</h5> </div>`;
+    document.querySelector(".toast").innerHTML  = `<div class="toast-header" style="background-color: #c4ff89;"> <img src="ok-24.png" class="me-auto"> <button type="button" class="btn-close" data-bs-dismiss="toast"></button> </div> <div class="toast-body" style="background-color: white; border-radius: 10px;"> <h5 style="text-align: center; font-size: 16px;">${message}</h5> </div>`;
     toastSetup();
 }
 
 
+// Open Yes / No toat
 function buttonToast(message){
-    document.querySelector(".toast").innerHTML  = `<div class="toast-header" style="background-color: #ADD8E6;"> <img src="info-24.png" class="me-auto"> <button type="button" class="btn-close" data-bs-dismiss="toast"></button> </div> <div class="toast-body" style="background-color: white; border-radius: 10px;"> <h5 style="text-align: center;">${message}</h5> <center> <button type="button" class="btn btn-success">Yes</button> <button type="button" class="btn btn-danger" style="margin-left: 15px;">No</button> </center> </div>`;
+    document.querySelector(".toast").innerHTML  = `<div class="toast-header" style="background-color: #ADD8E6;"> <img src="info-24.png" class="me-auto"> <button type="button" class="btn-close" data-bs-dismiss="toast"></button> </div> <div class="toast-body" style="background-color: white; border-radius: 10px;"> <h5 style="text-align: center; font-size: 16px;">${message}</h5> <center> <button type="button" class="btn btn-success">Yes</button> <button type="button" class="btn btn-danger" style="margin-left: 15px;">No</button> </center> </div>`;
     toastSetup();
 }
